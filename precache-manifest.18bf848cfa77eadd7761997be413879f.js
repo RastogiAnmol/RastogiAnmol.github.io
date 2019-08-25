3ca021100b34ec39b799f8bb6986cc9219fd2b3e
Anmol Rastogi <anmolamity@gmail.com>
2019-08-25 12:52:48 +0530

    tweaks

--- precache-manifest.18bf848cfa77eadd7761997be413879f.js
+++ precache-manifest.18bf848cfa77eadd7761997be413879f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d21829f6e5ad78d7fe5e6774847de99f",
+    "revision": "33045805488b22426bd843499dc34def",
     "url": "/index.html"
   },
   {
-    "revision": "c1980d912e32c6fa160c",
+    "revision": "ec3d56adcf285f9546f8",
     "url": "/static/css/main.34de6062.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.1d6e97e5.chunk.js"
   },
   {
-    "revision": "c1980d912e32c6fa160c",
-    "url": "/static/js/main.ad021c97.chunk.js"
+    "revision": "ec3d56adcf285f9546f8",
+    "url": "/static/js/main.7b5463b6.chunk.js"
   },
   {
     "revision": "fb1a82deb70a1ba250ab",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.572cea977bbac3eea2dc94ba85210c24.js"
+  "/precache-manifest.18bf848cfa77eadd7761997be413879f.js"
 );
 
 self.addEventListener('message', (event) => {
