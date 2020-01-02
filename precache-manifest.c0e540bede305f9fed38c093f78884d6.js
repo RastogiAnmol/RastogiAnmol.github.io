a219eebbfeb1690f116fd3f55ecb8dc3f8ecbd55
Anmol Rastogi <anmolamity@gmail.com>
2020-01-02 15:54:04 +0530

    updated files

---
+++ precache-manifest.c0e540bede305f9fed38c093f78884d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a61398b2d1cd63553b69de6216df46f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6a1c0e77abeb6e9665d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b66b8fc6a4310c55fef2",
+    "url": "/static/js/2.1d6e97e5.chunk.js"
+  },
+  {
+    "revision": "d6a1c0e77abeb6e9665d",
+    "url": "/static/js/main.f17409c8.chunk.js"
+  },
+  {
+    "revision": "fb1a82deb70a1ba250ab",
+    "url": "/static/js/runtime~main.8e14d2b1.js"
+  }
+]);
