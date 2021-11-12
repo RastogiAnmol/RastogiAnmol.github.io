aa2f59c5002db3b06bbf8178bcc63f0b37bee1a0
Anmol <anmolamity@gmail.com>
2021-11-12 23:01:10 +0530

    updated resume pdf and about me

--- precache-manifest.0b4e00e60972de0403ea500f5fbd45b5.js
+++ precache-manifest.0b4e00e60972de0403ea500f5fbd45b5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82dc4618d21a6cba960b403be64c484c",
+    "revision": "f7bfc8c9e27f0075d075c848274f099c",
     "url": "/index.html"
   },
   {
-    "revision": "de4994b1af9a54e5fa5f",
-    "url": "/static/css/main.34de6062.chunk.css"
+    "revision": "5f288d84845ebaea34ec",
+    "url": "/static/css/main.498d002a.chunk.css"
   },
   {
     "revision": "b66b8fc6a4310c55fef2",
     "url": "/static/js/2.1d6e97e5.chunk.js"
   },
   {
-    "revision": "de4994b1af9a54e5fa5f",
-    "url": "/static/js/main.c7fb478e.chunk.js"
+    "revision": "5f288d84845ebaea34ec",
+    "url": "/static/js/main.5478f21a.chunk.js"
   },
   {
     "revision": "fb1a82deb70a1ba250ab",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.377bf38d1a05cdbf9158f814c27104e4.js"
+  "/precache-manifest.0b4e00e60972de0403ea500f5fbd45b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
